d12a194d33b26a2a2f105213b8c9ed6736bf7e4b
vantutr <tranvantu06042001@gmail.com>
2025-03-10 20:47:35 +0700

    buoi 7

---
+++ buoi7/thuchanh2/dientich.js
@@ -0,0 +1,8 @@
+chieurong = prompt('Nhập chiều rộng:')
+chieucao = prompt('Nhập chiều cao:')
+
+let width = parseInt(chieurong);
+let height = parseInt(chieucao);
+
+let dientich = width * height;
+document.write("Diện tích hình chữ nhật = " + dientich);
